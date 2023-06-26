05a5e47d2d3278b443794c977e979776c033c4fb
Batunde-Dev <batunde.dev@gmail.com>
2023-06-26 01:34:31 -0700

    Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -5,11 +5,4 @@ exec { 'kill_killmenow_process':
   path        => '/usr/bin:/usr/sbin:/bin',
   onlyif      => 'pgrep killmenow',
   refreshonly => true,
-  subscribe   => Exec['start_killmenow_process'],
-}
-
-exec { 'start_killmenow_process':
-  command => 'pkill killmenow',
-  path    => '/usr/bin:/usr/sbin:/bin',
-  creates => 'path_to_killmenow_process',
 }
