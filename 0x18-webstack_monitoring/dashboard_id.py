ef6a154d3ef679ca11806626f0ac65c9903f7621
Batunde-Dev <batunde.dev@gmail.com>
2023-08-12 07:08:44 -0700

    Create a dashboard

---
+++ 0x18-webstack_monitoring/dashboard_id.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import requests
+
+# Set up the API endpoint and parameters
+api_url = "https://api.datadoghq.com/api/v1/dashboard/"
+api_key = "..."
+api_app_key = "..."
+headers = {'Content-type': 'application/json', 'DD-API-KEY': api_key, 'DD-APPLICATION-KEY': api_app_key}
+query_params = {'filter': 'host:221223-web-01'}
+
+# Make the API call to get the host details
+response = requests.get(api_url, headers=headers, params=query_params)
+
+# Check the response status code
+if response.status_code == 200:
+    # Parse the JSON response
+    response_json = response.json()
+    print(response_json)
